7a2e159439dfaef14a36a578de46f4e3e7463ec0
andrew-saeed <andrew-saeed.dev@outlook.com>
2024-11-23 00:00:00 +0000

    setup League\Container

--- php/framework/bootstrap/index.php
+++ php/framework/bootstrap/index.php
@@ -2,9 +2,18 @@
 require '../vendor/autoload.php';
 
 use Spatie\Ignition\Ignition;
+use League\Container\Container;
+
 use App\Core\App;
 
 // error_reporting(0);
+
+$container = new Container();
+$container->add('name', function() {
+    return 'name 1';
+});
+var_dump($container->get('name'));
+die();
 
 Ignition::make()->setTheme('dark')->register();
 
