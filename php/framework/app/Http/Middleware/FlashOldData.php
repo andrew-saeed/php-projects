03e4ecde66b99d9c758735e88f46e421e3fa2e59
andrew-saeed <andrew-saeed.dev@outlook.com>
2024-12-03 00:00:00 +0000

    persisting old form data

---
+++ php/framework/app/Http/Middleware/FlashOldData.php
@@ -0,0 +1,22 @@
+<?
+
+namespace App\Http\Middleware;
+
+use App\Core\Container;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class FlashOldData implements MiddlewareInterface {
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        Container::getInstance()->get(Session::class)->getFlashBag()->set('old', $request->getParsedBody());
+
+        return $response;
+    }
+}
--- php/framework/routes/web.php
+++ php/framework/routes/web.php
@@ -1,19 +1,21 @@
 <?
 
 use App\Http\Controllers\Auth\LoginController;
-use League\Route\Router;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\RegisterationController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\FlashOldData;
 use App\Http\Middleware\RedirectIfAuth;
 use App\Http\Middleware\RedirectIfGuest;
+use League\Route\Router;
 use League\Route\RouteGroup;
 use Psr\Container\ContainerInterface;
 
 return static function(Router $router, ContainerInterface $container) {
 
     $router->middleware($container->get('csrf'));
+    $router->middleware(new FlashOldData());
     
     $router->get('/', HomeController::class);
     $router->get('/users/{user}', UserController::class);
